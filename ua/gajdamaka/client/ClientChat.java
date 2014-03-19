ff01d3e4afdccd0f97811a058c2bf187051cb187
gajdamaka <gaydamaka.vov@gmail.com>
2014-03-19 19:57:18 +0200

    change read/write

--- ua/gajdamaka/client/ClientChat.java
+++ ua/gajdamaka/client/ClientChat.java
@@ -3,36 +3,58 @@ package ua.gajdamaka.client;
 import java.net.*;
 import java.io.*;
 
-public class ClientChat {
+public class ClientChat{
 
 	private static final String IP_ADRESS = "localhost";
 	private static final int PORT = 1234;
-
-
+	private BufferedReader in;
+	private PrintWriter out;
+	private Socket socket;
 
 	public ClientChat() {
 		try {
-			Socket socket = new Socket(IP_ADRESS, PORT);
+			socket = new Socket(IP_ADRESS, PORT);
 			System.out.println("Trying ..." + socket.getInetAddress().getHostAddress());
 			System.out.println("Connected to localhost");
-			BufferedReader in = new BufferedReader(new 
+			BufferedReader input = new BufferedReader(new 
    				InputStreamReader(System.in));
-			PrintWriter out = new PrintWriter(socket.getOutputStream(),true);
+
+			in  = new BufferedReader(
+				new InputStreamReader(socket.getInputStream()));
+
+			out = new PrintWriter(socket.getOutputStream(),true);
 			String msg = null;
-			while (true) {
-				msg = in.readLine();
+
+			Thread t = new Thread(new Runnable(){
+				public void run() {
+					String msg;
+    				try {
+      					while ((msg = in.readLine()) != null) {
+        					System.out.println("Server: " + msg);
+        					if (msg.equalsIgnoreCase("Bye")){
+        						in.close();
+        						out.close();
+          						socket.close();
+          						break;
+        					}
+      					}
+    				} catch (IOException ex) {
+      					ex.printStackTrace(System.err);
+    				}
+				}
+			});
+			t.start();
+			while ((msg = input.readLine()) != null) {
 				if (msg.equalsIgnoreCase("exit")) {
 					System.out.println("Connection closed by foreign host.");
+					out.println(msg);
 					break;
 				} else {
 					out.println(msg);
-					//out.flush();
 				}
 			}
-
 		} catch(IOException ex) {
 			ex.printStackTrace(System.err);
 		}
-	}  
-	
+	}
 }
