250059793c543b5bb19ff8bc3b3fd40d9990fbaf
gajdamaka <gaydamaka.vov@gmail.com>
2014-03-24 19:49:43 +0200

    input login user

--- ua/gajdamaka/Telnet.java
+++ ua/gajdamaka/Telnet.java
@@ -1,10 +1,14 @@
 package ua.gajdamaka;
 
 import ua.gajdamaka.client.*;
+import java.util.Scanner;
 
 public class Telnet {
 	
 	public static void main(String[] args) {
-		ClientChat client = new ClientChat();
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Input your name: ");
+		String login = scan.next();
+		ClientChat client = new ClientChat(login);
 	}
 }
