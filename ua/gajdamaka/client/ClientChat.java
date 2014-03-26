95966683b34bd8953dda82bbfd73aa7a6ba270ec
gajdamaka <gaydamaka.vov@gmail.com>
2014-03-26 21:58:16 +0200

    send message by serialization

--- ua/gajdamaka/client/ClientChat.java
+++ ua/gajdamaka/client/ClientChat.java
@@ -2,58 +2,60 @@ package ua.gajdamaka.client;
 
 import java.net.*;
 import java.io.*;
+import ua.gajdamaka.message.Message;
+
 
 public class ClientChat{
 
 	private static final String IP_ADRESS = "localhost";
 	private static final int PORT = 1234;
-	private BufferedReader in;
-	private PrintWriter out;
 	private Socket socket;
+	private ObjectInputStream in;
+	private ObjectOutputStream out;
 	private String login;
 
 	public ClientChat(String login) {
+			
 			try {
 				socket = new Socket(IP_ADRESS, PORT);
-				this.login = login; 
+				this.login = login;
 				System.out.println("Trying ..." + socket.getInetAddress().getHostAddress());
 				System.out.println("Connected to localhost");
-			
-				in  = new BufferedReader(
-					new InputStreamReader(socket.getInputStream()));
-
-				out = new PrintWriter(socket.getOutputStream(),true);
+				out = new ObjectOutputStream(socket.getOutputStream());
+				in  = new ObjectInputStream(socket.getInputStream());
+				if (socket != null) {
+					Thread t_out = new Thread(new Runnable(){
+						public void run() {
+							outputMessage();
+						}
+					});
+					Thread t_in = new Thread(new Runnable(){
+						public void run() {
+							inputMessage();
+						}
+					});
+					t_out.start();
+					t_in.start();
+				}
 			} catch(IOException ex) {
 				System.out.println("Error Connected");
-			}
-			if (socket != null) {
-				Thread t_out = new Thread(new Runnable(){
-					public void run() {
-						outputMessage();
-					}
-				});
-				Thread t_in = new Thread(new Runnable(){
-					public void run() {
-						inputMessage();
-					}
-				});
-				t_out.start();
-				t_in.start();
-			}			
+			}	
 	}
 
 	private void inputMessage() {
 		String msg = null;
 		BufferedReader input = new BufferedReader(new 
    				InputStreamReader(System.in));
+			
 		try {
 			while ((msg = input.readLine()) != null) {
+				Message message = new Message(login, msg, socket.getRemoteSocketAddress().toString());
 				if (msg.equalsIgnoreCase("exit")) {
 					System.out.println("Connection closed by foreign host.");
-					out.println(msg);
+					out.writeObject(message);
 					break;
 				} else {
-					out.println(login + ": " + msg);
+					out.writeObject(message);
 				}
 			}
 		} catch(IOException ex) {
@@ -62,17 +64,20 @@ public class ClientChat{
 	}
 
 	private void outputMessage() {
-		String msg;
+		Message mess;
     	try {
-      		while ((msg = in.readLine()) != null) {
-        		System.out.println(msg);
-        		if (msg.equalsIgnoreCase("Bye")){
+      		while ((mess = (Message) in.readObject()) != null) {
+        		System.out.println("Login: " + mess.getLogin() + "   " + mess.getDate());
+   				System.out.println(mess.getMessage() );
+        		if (mess.getMessage().equalsIgnoreCase("Bye")){
         			in.close();
         			out.close();
           			socket.close();
           			break;
         		}
-      		}
+      		} 
+    	} catch (ClassNotFoundException ex) {
+      		System.out.println("Class not found");
     	} catch (IOException ex) {
       		System.out.println("Error output stream");
     	}
