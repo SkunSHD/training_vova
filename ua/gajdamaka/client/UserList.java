cb29194e25f99ead3535a4688c169f80bb0d9b3e
gajdamaka <gaydamaka.vov@gmail.com>
2014-04-01 10:30:04 +0300

    authentication users

--- ua/gajdamaka/client/UserList.java
+++ ua/gajdamaka/client/UserList.java
@@ -3,11 +3,48 @@ package ua.gajdamaka.client;
 import java.util.*;
 import java.net.*;
 import java.io.*;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import ua.gajdamaka.message.Message;
 
 public class UserList {
 	
 	private Map<String, Client> users = new  HashMap<String, Client>();
+	private static Map<String, String> au = new  HashMap<String, String>();
+	private static final String PATH_CONFIG_FILE = "./ua/gajdamaka/users.xml";
+
+	static {
+		try {
+			File file = new File(PATH_CONFIG_FILE);
+			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+			DocumentBuilder db = dbf.newDocumentBuilder();
+			Document doc = db.parse(file);
+			doc.getDocumentElement().normalize();
+			NodeList nodeLst = doc.getElementsByTagName("user");
+			for (int s = 0; s < nodeLst.getLength(); s++) {
+				Node fstNode = nodeLst.item(s);
+			    if (fstNode.getNodeType() == Node.ELEMENT_NODE) {
+		        	Element fstElmnt = (Element) fstNode;
+					NodeList login_Lst = fstElmnt.getElementsByTagName("login");
+					Element loginNmElmnt = (Element) login_Lst.item(0);
+			    	NodeList loginNm = loginNmElmnt.getChildNodes();
+			    	NodeList pass_Lst = fstElmnt.getElementsByTagName("pass");
+			    	Element passNmElmnt = (Element) pass_Lst.item(0);
+			    	NodeList passNm = passNmElmnt.getChildNodes();
+			    	String login = ((Node) loginNm.item(0)).getNodeValue();
+			    	String pass = ((Node) passNm.item(0)).getNodeValue();
+			    	au.put(login, pass);
+				}	
+	  		}
+	  		System.out.println(au);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
 	public void addUser(String login, Client client) {
 		if (!users.containsKey(login)){
@@ -51,5 +88,14 @@ public class UserList {
 		} catch (IOException ex) {
 			System.out.println("Connected error!!!");
 		}
+	}
+
+	public boolean authentication(String login, String pass) {
+		for (Map.Entry<String, String> conf : au.entrySet()) {
+			if ((conf.getKey().equals(login)) && (conf.getValue().equals(pass))) {
+				return true;				
+			}
+		}
+		return false;
 	}
 }
--- ua/gajdamaka/server/ClientThread.java
+++ ua/gajdamaka/server/ClientThread.java
@@ -18,7 +18,7 @@ public class ClientThread extends Thread {
 	}
 
 	public void run() {
-		String login;
+		String login, pass;
 		try {
 			final ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
 			final ObjectInputStream in  = new ObjectInputStream(socket.getInputStream());
@@ -27,33 +27,48 @@ public class ClientThread extends Thread {
    				if (login != null) {
    					break;
    				}	
-   			}   			
-   			client = new Client(socket, in, out);
-   			ChatServer.getUserList().addUser(login, client);   			
-   			while (true) {
-   				if ((message = (Message) in.readObject()) != null) {
-   					if (message.getMessage().equalsIgnoreCase("exit")) {
-   						System.out.println("Client disconnected");
-   						Message msg = new Message("Server", "Bye", socket.getInetAddress().getHostAddress(), "Info mess");
-   						out.writeObject(msg);
-   						ChatServer.getUserList().deleteUser(login);
-   						in.close();
-   						out.close();
-   						socket.close(); 
-   						break;
-   					} else {
-   						System.out.println("Login: " + message.getLogin() + " (Status: " 
-                        + message.getStatus() + ")  " + message.getDate());
-   						System.out.println("Message :" +  message.getMessage());
-   						ChatServer.getMessageHistory().addMessage(message);
-   						this.broadcast(ChatServer.getUserList().getList(), message);
-   					}
-   				}	
    			}
+            while(true) {
+               pass = in.readUTF();
+               if (pass != null) {
+                  break;
+               }  
+            }
+            if (!ChatServer.getUserList().authentication(login, pass)) {
+               Message msg1 = new Message("Server", "Incorrectly entered data", socket.getInetAddress().getHostAddress(), "Info mess");
+               out.writeObject(msg1);
+               in.close();
+               out.close();
+               socket.close();
+            } else {
+      			client = new Client(socket, in, out);
+      			ChatServer.getUserList().addUser(login, client);   			
+      		 	while (true) {
+      	  			if ((message = (Message) in.readObject()) != null) {
+   	     				if (message.getMessage().equalsIgnoreCase("exit")) {
+   			   			System.out.println(login + " disconnected");
+                        Message msg3 = new Message("Server", login + " disconnected", socket.getInetAddress().getHostAddress(), "Info mess");
+                        this.broadcast(ChatServer.getUserList().getList(), msg3);
+                        Message msg4 = new Message("Server", "Bye", socket.getInetAddress().getHostAddress(), "Info mess");
+   						   out.writeObject(msg4);
+   						   ChatServer.getUserList().deleteUser(login);
+   						   in.close();
+   						   out.close();
+   						   socket.close(); 
+   						   break;
+                     } else {
+   						   System.out.println("Login: " + message.getLogin() + " (Status: " 
+                           + message.getStatus() + ")  " + message.getDate());
+   						   System.out.println("Message :" +  message.getMessage());
+   						   ChatServer.getMessageHistory().addMessage(message);
+   						   this.broadcast(ChatServer.getUserList().getList(), message);
+                     }
+                  }	
+   			   }
+            }
    		} catch (ClassNotFoundException ex) {
    			System.out.println("Class not found");
    		} catch (IOException ex) {
-            ex.printStackTrace(System.err);
 			   System.out.println("Run Connected error!!!");
 		}
 
