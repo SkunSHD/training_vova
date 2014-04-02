691755635b23a12b8bb46f5fe9a8e2f5448c17cb
Olesiy Slyshkov <Oleksiy_Slyshkov@epam.com>
2014-04-02 12:16:52 +0300

    TRAINING: Review comments for string sorter realization

--- SortedString.java
+++ SortedString.java
@@ -1,14 +1,22 @@
 import java.util.*;
-
+//fixme ever use package system for all classes
+//todo second condition not accepted
+//todo third condition accepted particle(see comment to getting parameter)
+//todo fourth condition incorrect you can use number of character in word
 public class SortedString {
-	
+
+    //todo Arrays in java very unusable thing use some Collection instead of Array
+    //fixme array it's not good name for anything
 	private String[] array;
-	private boolean flag_length;	
+    //fixme boolean flag not good in that case, you can create comparator instead of flag and just use it in to sorting without any additional methods
+	private boolean flag_length;
+    //fixme unused variables bad style
 	private boolean flag_number;
 	private int index;
 
 	public SortedString(String[] args) {
 		if (args.length != 0) {
+            //fixme read about system variable in Java(like -Dxmx=512M, -Djava.version)
 			if (args[0].equals("-D")) {
 				array = Arrays.copyOfRange(args ,1, args.length);
 				flag_length = true;
@@ -17,6 +25,8 @@ public class SortedString {
 			}
 			print("Default lines");
 		} else {
+            //fixme you done your program without any choice for user.
+            // fixme Try to imagine: your OS after start just showed message "You not input parameter. Try again" and turn  off itself after that
 			System.out.println("You not input parameter. Try again");
 		}
 
